9cff1dcd2982459be21b6791fa80e4dd7fe109c0
ashfiyakapali <ashfiyakapali@gmail.com>
2025-05-06 15:19:02 +0530

    save it in master

--- Employee.java
+++ Employee.java
@@ -3,20 +3,26 @@ public class Employee {
     int id;
     double salary;
     String phone_no;
+    String branch;
 
-    public Employee(String name, int id,double salary,String phone_no){
+    public Employee(String name, int id,double salary,String phone_no,String branch){
         this.name=name;
         this.id=id;
         this.salary=salary;
+        this.phone_no=phone_no;
+        this.branch=branch;
     }
     public void displayInfo(){
         System.out.println("Employee ID: " + id);
         System.out.println("Employee Name: " + name);
         System.out.println("Salary: $" + salary);
+        System.out.println("phone_no" + phone_no);
+        System.out.println("branch: " + branch);
+        
+
     }
     public static void main(String[] args) {
-        Employee emp1 = new Employee("XYZ", 101, 50000.0, "1234567899");
+        Employee emp1 = new Employee("XYZ", 101, 50000.0, "1234567899", "BCA");
         emp1.displayInfo();
     }
-
 }
